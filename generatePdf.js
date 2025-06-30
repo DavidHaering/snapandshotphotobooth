0f76f9413bbefecb9963f4e59946f483f321903a
David <dhaering95@gmail.com>
2025-06-30 10:09:58 +0200

    generatePdf.js

--- generatePdf.js
+++ generatePdf.js
@@ -955,12 +955,12 @@ async function uploadPdfToGCS(formData) {
 
     checkPageBreak();
     doc.text(`Votre adresse email: ${email}`, margeGauche, y);
-    doc.moveDown()
+    y += interligne * 2;
 
     checkPageBreak();
     doc.text(`Votre numéro de téléphone: ${telephone}`, margeGauche, y);
 
-    doc.moveDown(1)
+    y += interligne * 3
 
     //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
